054702204017868f028044d0eb94413d040f9fd8
Mason Stricklin <mason@0x1A4.local>
2016-01-26 05:11:09 -0800

    made it all caps

--- cs191/cs191/wsgi.py
+++ cs191/cs191/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cs191 project.
+WSGI config for CS191 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs191.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CS191.settings")
 
 application = get_wsgi_application()
