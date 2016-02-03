12f94dbe428c875bf8ce0fe06d97830aa42a9b73
Mason Stricklin <mason@0x1A4.local>
2016-02-03 02:15:26 -0800

    created simple post system

--- CS191/posts/models.py
+++ CS191/posts/models.py
@@ -2,18 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+# Uncomment the following line if you are having issues with python2
+# @python_2_unicode_compatible
 class Post(models.Model):
     title = models.CharField(max_length=140)
     date = models.DateTimeField('date submitted')
     text = models.TextField()
 
-    # If a URL is required, the frontend will ensure it gets created with one
+    ARTICLE = 'AP' # Text Post (should not have url)
+    VIDEO = 'VP' # Video Post (url required)
+    PHOTO = 'PP' # Single photo (url requird)
+    SLIDES = 'SP' # Multiple photos (url required)
+
+    # URL requirement monitored at form level not model level
     POST_TYPE_CHOICES = (
-        ('AP', 'Article Post'), # Text Post (should not have url)
-        ('VP', 'Video Post'), # Video Post (url required)
-        ('PP', 'Photo Post'), # Single photo (url requird)
-        ('SP', 'Slideshow Post'), # Multiple photos (url required)
+        ('AP', 'Article Post'),
+        ('VP', 'Video Post'),
+        ('PP', 'Photo Post'),
+        ('SP', 'Slideshow Post'),
     )
-    post_type = models.URLField(max_length=2, choices=POST_TYPE_CHOICES,
+    post_type = models.CharField(max_length=2, choices=POST_TYPE_CHOICES,
                                 blank=False, default='AP')
-    url = models.URLField(default = None)
+    url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- CS191/posts/views.py
+++ CS191/posts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
 
 def index(request):
-    return HttpResponse("posts index")
+    latest_post_list = Post.objects.order_by('date')[:5]
+    context = {
+        'latest_post_list': latest_post_list,
+    }
+    return render(request, 'posts/index.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/detail.html', context)
