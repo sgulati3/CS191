311f2bd381222dfbbad058de29e54b3df6de6ccb
Mason Stricklin <mason@0x1A4.local>
2016-02-02 22:58:31 -0800

    created first model

--- CS191/posts/models.py
+++ CS191/posts/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    date = models.DateTimeField('date submitted')
+    text = models.TextField()
+
+    # If a URL is required, the frontend will ensure it gets created with one
+    POST_TYPE_CHOICES = (
+        ('AP', 'Article Post'), # Text Post (should not have url)
+        ('VP', 'Video Post'), # Video Post (url required)
+        ('PP', 'Photo Post'), # Single photo (url requird)
+        ('SP', 'Slideshow Post'), # Multiple photos (url required)
+    )
+    post_type = models.URLField(max_length=2, choices=POST_TYPE_CHOICES,
+                                blank=False, default='AP')
+    url = models.URLField(default = None)
