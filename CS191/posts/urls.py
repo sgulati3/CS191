2990067f9f51b9ee576c19895be086b9ead23210
Mason Stricklin <mason@DN51qg4b.SUNet>
2016-02-18 21:10:04 -0800

    users can now submit new posts

--- CS191/posts/urls.py
+++ CS191/posts/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 app_name= 'posts'
 urlpatterns = [
-    url(r'^$', views.index, namespace='index'),
-    url(r'^(?P<post_id>[0-9]+)$', views.detail, namespace='detail'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<post_id>[0-9]+)$', views.detail, name='detail'),
+    url(r'^create$', views.create, name='create'),
 ]
